77fdbff9024c299bb7bbe969e907ce23a0e1f93f
Enduren <dtennison79@gmail.com>
2021-10-26 13:57:54 -0500

    initial commit

---
+++ Sugar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SoloLearn
+{
+    class Sugar
+    {
+        static void Main(string[] args)
+        {
+            int answer = 4;
+            string response;
+
+            if (answer < 9)
+            {
+                response = answer + " is less than nine";
+            }
+            else
+            {
+                response = answer + "greater than or equal to nine";
+            }
+            Console.WriteLine(response);
+        }
+    }
